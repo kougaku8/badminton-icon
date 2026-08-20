01975c14bfcdad3b6365dc40f750829aabb0b233
kougaku8 <huange@hotmail.co.jp>
2026-08-20 20:56:21 +0900

    Refactor Firebase messaging service worker
    Removed Firebase initialization and updated notification title and body.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,55 +1,16 @@
-importScripts(
-  "https://www.gstatic.com/firebasejs/12.18.0/firebase-app-compat.js"
-);
-
-importScripts(
-  "https://www.gstatic.com/firebasejs/12.18.0/firebase-messaging-compat.js"
-);
-
-firebase.initializeApp({
-  apiKey: "AIzaSyD6uQto_5BYZJDgkaI8CBGaOQkXMgrCk",
-  authDomain: "badminton-notification.firebaseapp.com",
-  projectId: "badminton-notification",
-  storageBucket: "badminton-notification.firebasestorage.app",
-  messagingSenderId: "419794096766",
-  appId: "1:419794096766:web:9dd990fe827719d149fe2f"
-});
-
-const messaging = firebase.messaging();
-
-
 messaging.onBackgroundMessage(function(payload) {
 
   console.log(
-    "[firebase-messaging-sw.js] Background message:",
+    "[FCM] Background message received",
     payload
   );
 
-  const title =
-    payload.notification &&
-    payload.notification.title
-      ? payload.notification.title
-      : "🏸 羽球活動";
-
-  const body =
-    payload.notification &&
-    payload.notification.body
-      ? payload.notification.body
-      : "新しい通知があります";
-
-
   self.registration.showNotification(
-    title,
+    "🏸 テスト通知",
     {
-      body: body,
-
+      body: "Firebase通知が届きました",
       icon:
-        "https://kougaku8.github.io/badminton-icon/heian-bado-yoyaku-icon_512.png",
-
-      badge:
-        "https://kougaku8.github.io/badminton-icon/heian-bado-yoyaku-icon_512.png",
-
-      requireInteraction: false
+        "https://kougaku8.github.io/badminton-icon/heian-bado-yoyaku-icon_512.png"
     }
   );
 
